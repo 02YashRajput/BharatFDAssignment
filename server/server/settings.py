f6e2da93ddf02acc6fea0121db72655425cf0f3e
Yash Rajput <02yashrajput@gmail.com>
2025-02-02 10:21:46 +0530

    feat:added online redis

--- server/server/settings.py
+++ server/server/settings.py
@@ -142,15 +142,17 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1', 
+        'LOCATION': os.getenv('REDIS_URL'), 
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         },
     }
 }
+
 
 ALLOWED_HOSTS = [
     'bharatfdassignment-97kk.onrender.com',
